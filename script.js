ab01e580b20de8242258aac2e7d63a666d01dfa2
Selina Yeshanew <88796374+seli1212@users.noreply.github.com>
2023-07-03 11:00:07 +0300

    commit

--- script.js
+++ script.js
@@ -14,4 +14,33 @@ async function searchImages(){
         const data =await response.json()
 
         const results=data.results
+        if(page===1){
+                searchResults.innerHTML=""
+        }
+        results.map((result)=>{
+
+            const imageWrapper=document.createElement('div')
+            imageWrapper.classList.add("search-result")  
+            const  image=document.createElement('img')
+             image.src=result.urls.small
+             image.alt=result.alt_description
+             const imageLink=document.createElement('a')
+             imageLink.href=result.links.html
+             imageLink.target="_blank"
+             imageLink.textContent=result.alt_description
+             
+             imageWrapper.appendChild(image)
+             imageWrapper.appendChild(imageLink)
+             imageWrapper.appendChild(imageWrapper)
+
+        })
+        page++
+        if(page>1 ){
+                showMore.style.display="block"
+        }
 }
+formElement.addEventListener("submit",(event)=>{
+        event.preventDefault()
+        page=1;
+        searchImages()
+})
